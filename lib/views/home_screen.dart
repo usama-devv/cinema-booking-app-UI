edabe1e9a3f3958b09cc6610a20d4127f1274aa6
Usama Masood <134695768+usama-devv@users.noreply.github.com>
2024-09-28 14:48:01 +0500

    initial commit

--- lib/views/home_screen.dart
+++ lib/views/home_screen.dart
@@ -1,14 +1,62 @@
 import 'package:cinema_booking_app_ui/utils/constants/colors.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: appBackgroundColor,
+  State<HomeScreen> createState() => _HomeScreenState();
+}
 
+class _HomeScreenState extends State<HomeScreen> {
+  List<IconData> bottomIcons = [
+    Icons.home_filled,
+    CupertinoIcons.compass_fill,
+    CupertinoIcons.ticket_fill,
+    Icons.person_rounded
+  ];
+  int currentIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: appBackgroundColor,
+      bottomNavigationBar: SizedBox(
+        height: 100,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: List.generate(
+            bottomIcons.length,
+            (index) => GestureDetector(
+              onTap: () {},
+              child: Stack(
+                children: [
+                  AnimatedContainer(
+                    duration: const Duration(milliseconds: 300),
+                    height: currentIndex == index ? 25 : 0,
+                    width: currentIndex == index ? 25 : 0,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      boxShadow: [
+                        BoxShadow(
+                            color: Colors.white.withOpacity(0.1),
+                            spreadRadius: currentIndex == index ? 10 : 0,
+                            blurRadius: currentIndex == index ? 15 : 0),
+                      ],
+                    ),
+                  ),
+                  Icon(
+                    bottomIcons[index],
+                    color: currentIndex == index
+                        ? Colors.white
+                        : Colors.white.withOpacity(0.3),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
