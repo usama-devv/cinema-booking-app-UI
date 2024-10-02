54f4ad33fdfba8303726ca301438929c06d32bb2
Usama Masood <134695768+usama-devv@users.noreply.github.com>
2024-10-02 20:27:00 +0500

    updated

--- lib/views/reservation_screen.dart
+++ lib/views/reservation_screen.dart
@@ -79,7 +79,6 @@ class _ReservationScreenState extends State<ReservationScreen> {
             ),
           ),
           const SizedBox(height: 20),
-
           /// For Movie Seats
           Column(
             children: [
@@ -147,25 +146,53 @@ class _ReservationScreenState extends State<ReservationScreen> {
               ],
             ),
           const SizedBox(height: 35),
-          SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: Row(
-              children: [
-                ...List.generate(
-                  availableTime.length,
-                  (index) => GestureDetector(
-                    onTap: (){
-                      setState(() {
-                        selectedSeats.clear();
-                      });
-                    },
-                  ),
-                ),
-              ],
-            ),
-          ),
+          // SingleChildScrollView(
+          //   scrollDirection: Axis.horizontal,
+          //   child: Row(
+          //     children: [
+          //       ...List.generate(
+          //         availableTime.length,
+          //         (index) => GestureDetector(
+          //           onTap: (){
+          //             setState(() {
+          //               selectedSeats.clear();
+          //             });
+          //           },
+          //         ),
+          //       ),
+          //     ],
+          //   ),
+          // ),
         ],
       ),
     );
   }
+}
+class ClipShadow extends CustomClipper<Path>{
+  @override
+  Path getClip(Size size) {
+    var path = Path();
+    path.lineTo(0, size.height);
+    path.quadraticBezierTo(size.width / 2, -20, size.width, size.height);
+    path.lineTo(size.width, size.height - 5);
+    path.quadraticBezierTo(size.width / 2, -25, 0, size.height -5);
+    path.close();
+    return path;
+  }
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) => false;
+}
+class ClipBorder extends CustomClipper<Path>{
+  @override
+  Path getClip(Size size){
+    var path = Path();
+    path.lineTo(0, size.height);
+    path.lineTo(size.width, size.height);
+    path.lineTo(size.width, size.height - 30);
+    path.quadraticBezierTo(size.width / 2, -20, 0, size.height - 30);
+    path.close();
+    return path;
+  }
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) => false;
 }
