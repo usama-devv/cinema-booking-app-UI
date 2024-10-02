e153ae508f0f39285ec3f22540a753f78b13c5a7
Usama Masood <134695768+usama-devv@users.noreply.github.com>
2024-10-02 22:58:27 +0500

    updated

--- lib/views/reservation_screen.dart
+++ lib/views/reservation_screen.dart
@@ -168,31 +168,4 @@ class _ReservationScreenState extends State<ReservationScreen> {
     );
   }
 }
-class ClipShadow extends CustomClipper<Path>{
-  @override
-  Path getClip(Size size) {
-    var path = Path();
-    path.lineTo(0, size.height);
-    path.quadraticBezierTo(size.width / 2, -20, size.width, size.height);
-    path.lineTo(size.width, size.height - 5);
-    path.quadraticBezierTo(size.width / 2, -25, 0, size.height -5);
-    path.close();
-    return path;
-  }
-  @override
-  bool shouldReclip(CustomClipper<Path> oldClipper) => false;
-}
-class ClipBorder extends CustomClipper<Path>{
-  @override
-  Path getClip(Size size){
-    var path = Path();
-    path.lineTo(0, size.height);
-    path.lineTo(size.width, size.height);
-    path.lineTo(size.width, size.height - 30);
-    path.quadraticBezierTo(size.width / 2, -20, 0, size.height - 30);
-    path.close();
-    return path;
-  }
-  @override
-  bool shouldReclip(CustomClipper<Path> oldClipper) => false;
-}
+
